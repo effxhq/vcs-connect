62b56686ab3338b830564828e881d9dcb82b66b6
Mya Pitzeruse <mya@effx.com>
2020-10-27 15:12:25 -0500

    ch203: stubbed out core indexing logic

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/effxhq/vcs-connect
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.3.0
+	github.com/urfave/cli/v2 v2.2.0
+	gopkg.in/src-d/go-billy.v4 v4.3.2
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
---
+++ internal/run/consumer_test.go
@@ -0,0 +1,41 @@
+package run_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/effxhq/vcs-connect/internal/run"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConsumer_SetupFS(t *testing.T) {
+	tmp := t.TempDir()
+	defer os.Remove(tmp)
+
+	c := &run.Consumer{}
+
+	// TODO: Use self instead of a separate random repo?
+	err := c.SetupFS(tmp, "https://github.com/effxhq/effx-sync-action.git")
+	require.NoError(t, err)
+
+	_, err = os.Stat(path.Join(tmp, "LICENSE"))
+	require.NoError(t, err)
+
+	_, err = os.Stat(path.Join(tmp, "README.md"))
+	require.NoError(t, err)
+}
+
+func TestConsumer_FindEffxYAML(t *testing.T) {
+	c := &run.Consumer{}
+
+	workDir := path.Join("..", "..", "hack", "run")
+
+	files, err := c.FindEffxYAML(workDir)
+	require.NoError(t, err)
+
+	require.Len(t, files, 2)
+	require.Contains(t, files, "effx.yaml")
+	require.Contains(t, files, "prefixed.effx.yaml")
+}
